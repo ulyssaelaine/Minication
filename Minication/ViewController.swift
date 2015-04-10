701dbb3a430cd2ad2153d85ff43b49feacff14ff
Elaine Reyes <elaineanxa@manuelanxa.local>
2015-04-10 18:04:24 +0800

    first commit

---
+++ Minication/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Minication
+//
+//  Created by Elaine Reyes on 4/10/15.
+//  Copyright (c) 2015 UlyssaElaine. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
